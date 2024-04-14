f38ce881884e9e0b57ab5eb80c04e720126a9d89
Agnieszka Roksz <5009238@teb-gdansk.ad>
2024-04-14 13:05:42 +0200

    Add input (to fix)

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import Task from "./components/Task.js"
 
 export default function App() {
+  const handleTask = () => { }
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={style.sectionTitle}>Tytuł taska</Text>
-        <View style={styles.items}></View>
+        <View style={styles.items}>
+          <Task text="Test 1"></Task>
+        </View>
     </View>
-    </View>
+
+        <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+
+        <TextInput style={styles.input} placeholder={'Write a Task'} />
+        <TouchableOpacity onPress={() => handleTask ()}>
+        <View style={styles.addWraper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+        </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
   );
 }
 
@@ -27,5 +40,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
-  }
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
--- components/Task.js
+++ components/Task.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet,  } from 'react-native'
 
 const Task = () => {
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
                 <View style={styles.square}></View>
-                <Text style={styles.itemText}>{ProgressBarAndroidComponent.text}</Text>
+                <Text style={styles.itemText}>{props.text}</Text>
            </View>
            <View style={styles.circular}></View>
         </View>
@@ -14,7 +14,41 @@ const Task = () => {
 }
 
 const styles = StyleSheet.create({
+item: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+},
+itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+},
+square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#55bcf6',
 
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+
+},
+itemText: {
+    maxWidth: '80%',
+},
+circular: {
+    width: 12,
+    height: 12,
+    backgroundColor: '#55bcf6',
+    borderWidth: 2,
+    borderRadius: 5,
+
+}
 
 })
 
